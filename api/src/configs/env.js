ed969a8c022a842266135beb1a8d586221d42a75
okezieobi <obiedereokezie@gmail.com>
2020-03-18 14:16:59 +0100

    [Chore#170699449] Fixed text for api

--- api/src/configs/env.js
+++ api/src/configs/env.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default class Env {
   constructor() {
-    this.postgresURL = process.env.POSTGRE_URL;
+    this.postgresURL = process.env.POSTGRES_URL;
     this.appPort = process.env.PORT;
     this.jwtSecret = process.env.SECRET;
     this.herokuPostgresURL = process.env.DATABASE_URL;
