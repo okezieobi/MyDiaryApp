f4ffbe76da6a32ee858745f1f6d158fe9f49ec48
okezieobi <obiedereokezie@gmail.com>
2020-04-23 16:26:25 +0100

    Updating seeders

--- mocks/users.js
+++ mocks/users.js
@@ -2,11 +2,14 @@ import { User } from '../api/src/models/user';
 
 export default [
   {
-    id: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJiNTIyMGE4OS1jZDgwLTQ0ZGYtYjBjOS1kY2FkZmM1NzU4M2YiLCJpYXQiOjE1ODc0MTY2MzMsImV4cCI6MTU4NzUwMzAzM30.7Pi32cEzfRdvoP09IxLgGSaJmj_eYQ8ZQ4eqs2vAhIg',
+    id: '18ae5a5b-4c5f-410e-aef1-c0c800cf45f9',
     fullName: 'Frank Okezie',
     username: 'Obiedere',
     email: 'foobar@mail.com',
-    password: User.hashString('456789Lovely'),
+    password: 'dkdkdkfjkfksfllslfllslfkskfkslsfkslsfmnmcmcnkdld',
+    type: 'Client',
+    createdAt: new Date(),
+    updatedAt: new Date(),
   },
   {
     id: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJiNTIyMGE4OS1jZDgwLTQ0ZGYtYjBjOS1kY2FkZmM1NzU4M2YiLCJpYXQiOjE1ODc0MTY2MzMsImV4cCI6MTU4NzUwMzAzM30.7Pi32cEzfRdvoP09IxLgGSaJmj_eYQ8ZQ4eqs2vAhIh',
