5ae4958414f89a02895643d1dfd08d474d1c813a
okezieobi <obiedereokezie@gmail.com>
2020-06-07 19:11:57 +0100

    Pushed for windows env

---
+++ client/src/components/layouts/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Auth from '../templates/Auth';
+import Form from '../templates/Form';
+
+const useStyles = makeStyles(() => ({
+  root: {
+
+  },
+}));
+
+export default function () {
+
+}
--- client/src/components/templates/Form.jsx
+++ client/src/components/templates/Form.jsx
@@ -1,1 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+function FormFactory({ inputs }) {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      {
+              inputs.map(({ id, label, variant }) => (
+                <TextField id={id} label={label} variant={variant} />
+              ))
+          }
+    </form>
+  );
+}
+
+FormFactory.propTypes = {
+  inputs: PropTypes.arrayOf.isRequired,
+};
