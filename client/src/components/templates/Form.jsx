596d604cdf19a87814b6f36552167824874ef86b
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-07-17 09:30:42 +0100

    Added Form component

--- client/src/components/templates/Form.jsx
+++ client/src/components/templates/Form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField } from '@material-ui/core';
+import { TextField, Button, ButtonGroup } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,20 +12,30 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function FormFactory({ inputs }) {
+function FormFactory({ inputs, buttons }) {
   const classes = useStyles();
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      {
+      <div>
+        {
               inputs.map(({ id, label, variant }) => (
                 <TextField id={id} label={label} variant={variant} />
               ))
           }
+      </div>
+      <ButtonGroup>
+        {
+          buttons.map(({ id, text }) => (
+            <Button id={id}>{text}</Button>
+          ))
+        }
+      </ButtonGroup>
     </form>
   );
 }
 
 FormFactory.propTypes = {
   inputs: PropTypes.arrayOf.isRequired,
+  buttons: PropTypes.arrayOf.isRequired,
 };
